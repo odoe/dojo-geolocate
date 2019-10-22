9c049860b65d8fdcf9f02d7b9b182612231a06f8
odoe <rrubalcava@esri.com>
2019-10-22 09:20:33 -0700

    geolocate widget

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,15 @@
 import { tsx, create } from '@dojo/framework/core/vdom';
-import theme from '@dojo/framework/core/middleware/theme';
-import Outlet from '@dojo/framework/routing/Outlet';
-import dojo from '@dojo/themes/dojo';
 
-import Menu from './widgets/Menu';
-import Home from './widgets/Home';
-import About from './widgets/About';
-import Profile from './widgets/Profile';
+import Geolocate from './widgets/Geolocate';
 
 import * as css from './App.m.css';
 
-const factory = create({ theme });
+const factory = create();
 
-export default factory(function App({ middleware: { theme } }) {
-	if (!theme.get()) {
-		theme.set(dojo);
-	}
+export default factory(function App() {
 	return (
 		<div classes={[css.root]}>
-			<Menu />
-			<div>
-				<Outlet key="home" id="home" renderer={() => <Home />} />
-				<Outlet key="about" id="about" renderer={() => <About />} />
-				<Outlet key="profile" id="profile" renderer={() => <Profile username="Dojo User" />} />
-			</div>
+			<Geolocate />
 		</div>
 	);
 });
--- src/widgets/Geolocate.tsx
+++ src/widgets/Geolocate.tsx
@@ -2,12 +2,19 @@ import { create, tsx } from "@dojo/framework/core/vdom";
 
 import geolocation from "../middleware/geolocation";
 
+import * as css from "./styles/Geolocate.m.css";
+
 const factory = create({ geolocation });
 
 const Geolocate = factory(function ({ middleware: { geolocation } }) {
   const { latitude, longitude } = geolocation();
   return (
-    <label>Lat: {latitude.toFixed(3)} | Lon: {longitude.toFixed(3)}</label>
+    <div classes={[css.root]}>
+      <span>
+        <svg classes={[css.icon]} xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32"><path d="M15.5 0C10.806 0 7 1.817 7 6.543v16.901L15.5 32l8.5-8.556V6.543C24 1.817 20.194 0 15.5 0zm0 15.1a4.6 4.6 0 1 1 4.6-4.6 4.599 4.599 0 0 1-4.6 4.6z"/></svg>
+      </span>
+      <label>Lat: {latitude.toFixed(3)} | Lon: {longitude.toFixed(3)}</label>
+    </div>
   );
 });
 
